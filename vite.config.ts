bcc115fbbc4ca4f92ca479471c26c2304060e8f9
ljhastr <lukehookemlh@gmail.com>
2025-07-26 14:35:21 +0100

    FIX: Can't pull orign main so applied stash

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        content: resolve(__dirname, 'src/content.ts'),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'content') return 'content.js';
+          return '[name].js';
+        },
+      },
+    },
+    emptyOutDir: true,
+    outDir: 'dist',
+  },
+});
